90c50901e4e02640d9e31184e4e8a180eaf60c97
ayan4m1 <andrew@bulletlogic.com>
2020-07-12 16:56:41 -0400

    add fields to queries and make queries conditionally executed

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -6,43 +6,239 @@ query {
   orders(first: 100) {
     edges {
       node {
-        id
-        name
-        createdAt
-        customer {
-          displayName
-          email
+        billingAddress {
+          address1
+          address2
+          city
+          company
+          country
+          countryCodeV2
+          firstName
+          id
+          lastName
+          latitude
+          longitude
+          name
+          phone
+          province
+          provinceCode
+          zip
         }
-        fullyPaid
-        displayFulfillmentStatus
-        totalTaxSet {
+        billingAddressMatchesShippingAddress
+        canMarkAsPaid
+        canNotifyCustomer
+        cancelReason
+        cancelledAt
+        capturable
+        cartDiscountAmountSet {
           shopMoney {
             amount
+            currencyCode
           }
         }
-        totalShippingPriceSet {
+        clientIp
+        closed
+        closedAt
+        confirmed
+        createdAt
+        currencyCode
+        currentTotalDutiesSet {
           shopMoney {
             amount
+            currencyCode
+          }
+        }
+        customer {
+          acceptsMarketing
+          acceptsMarketingUpdatedAt
+          averageOrderAmountV2 {
+            amount
+            currencyCode
+          }
+          canDelete
+          createdAt
+          displayName
+          email
+          firstName
+          hasNote
+          hasTimelineComment
+          id
+          lastName
+          legacyResourceId
+          lifetimeDuration
+          locale
+          marketingOptInLevel
+          note
+          ordersCount
+          phone
+          state
+          tags
+          taxExempt
+          totalSpentV2 {
+            amount
+            currencyCode
+          }
+          updatedAt
+          validEmailAddress
+          verifiedEmail
+        }
+        discountCode
+        displayFinancialStatus
+        displayFulfillmentStatus
+        edited
+        email
+        fulfillable
+        fullyPaid
+        hasTimelineComment
+        id
+        legacyResourceId
+        merchantEditable
+        name
+        netPaymentSet {
+          shopMoney {
+            amount
+            currencyCode
+          }
+        }
+        note
+        originalTotalDutiesSet {}
+          shopMoney {
+            amount
+            currencyCode
+          }
+        }
+        originalTotalPriceSet {
+          shopMoney {
+            amount
+            currencyCode
+          }
+        }
+        paymentGatewayNames
+        phone
+        processedAt
+        refundDiscrepancySet {
+          shopMoney {
+            amount
+            currencyCode
+          }
+        }
+        refundable
+        requiresShipping
+        restockable
+        subtotalLineItemsQuantity
+        subtotalPriceSet {
+          shopMoney {
+            amount
+            currencyCode
+          }
+        }
+        tags
+        taxesIncluded
+        totalCapturableSet {
+          shopMoney {
+            amount
+            currencyCode
+          }
+        }
+        totalDiscountsSet {
+          shopMoney {
+            amount
+            currencyCode
+          }
+        }
+        totalOutstandingSet {
+          shopMoney {
+            amount
+            currencyCode
+          }
+        }
+        totalPriceSet {
+          shopMoney {
+            amount
+            currencyCode
           }
         }
         totalReceivedSet {
           shopMoney {
             amount
+            currencyCode
           }
         }
+        totalRefundedSet {
+          shopMoney {
+            amount
+            currencyCode
+          }
+        }
+        totalShippingPriceSet {
+          shopMoney {
+            amount
+            currencyCode
+          }
+        }
+        totalTaxSet {
+          shopMoney {
+            amount
+            currencyCode
+          }
+        }
+        totalTipReceived {
+          amount
+          currencyCode
+        }
+        totalWeight
+        unpaid
+        updatedAt
         lineItems(first: 50) {
           edges {
             node {
               id
               quantity
               product {
+                createdAt
+                description
+                descriptionHtml
+                giftCardTemplateSuffix
+                handle
+                hasOnlyDefaultVariant
+                hasOutOfStockVariants
                 id
-                title
+                isGiftCard
+                legacyResourceId
+                mediaCount
+                onlineStorePreviewUrl
+                onlineStoreUrl
+                options {
+                  id
+                  name
+                  position
+                  values
+                }
                 priceRange {
+                  minVariantPrice {
+                    amount
+                    currencyCode
+                  }
                   maxVariantPrice {
                     amount
+                    currencyCode
                   }
                 }
+                productType
+                publishedAt
+                seo {
+                  description
+                  title
+                }
+                storefrontId
+                tags
+                templateSuffix
+                title
+                totalInventory
+                totalVariants
+                tracksInventory
+                updatedAt
+                vendor
               }
               variant {
                 id
@@ -62,7 +258,71 @@ query {
   products(first: 100) {
     edges {
       node {
+        createdAt
+        description
+        descriptionHtml
+        giftCardTemplateSuffix
+        handle
+        hasOnlyDefaultVariant
+        hasOutOfStockVariants
         id
+        isGiftCard
+        legacyResourceId
+        mediaCount
+        onlineStorePreviewUrl
+        onlineStoreUrl
+        options {
+          id
+          name
+          position
+          values
+        }
+        priceRange {
+          minVariantPrice {
+            amount
+            currencyCode
+          }
+          maxVariantPrice {
+            amount
+            currencyCode
+          }
+        }
+        productType
+        publishedAt
+        seo {
+          description
+          title
+        }
+        storefrontId
+        tags
+        templateSuffix
+        title
+        totalInventory
+        totalVariants
+        tracksInventory
+        updatedAt
+        variants(first: 50) {
+          availableForSale
+          barcode
+          compareAtPrice
+          createdAt
+          displayName
+          id
+          inventoryPolicy
+          inventoryQuantity
+          legacyResourceId
+          position
+          price
+          sku
+          storefrontId
+          taxCode
+          taxable
+          title
+          updatedAt
+          weight
+          weightUnit
+        }
+        vendor
       }
     }
   }
@@ -72,34 +332,39 @@ query {
 exports.sourceNodes = async ({ actions, reporter }, options) => {
   try {
     const { createNode } = actions;
+    const { queries } = options;
 
-    const orders = await createQuery(
-      ordersQuery,
-      {
-        LineItem: 'lineItems'
-      },
-      options,
-      reporter
-    );
+    if (!queries || queries.orders) {
+      const orders = await createQuery(
+        ordersQuery,
+        {
+          LineItem: 'lineItems'
+        },
+        options,
+        reporter
+      );
 
-    reporter.info(
-      `gatsby-source-shopify-graphql fetched ${orders.length} orders`
-    );
-    orders.forEach((order) => createNode(transformOrderNode(order)));
+      reporter.info(
+        `gatsby-source-shopify-graphql fetched ${orders.length} orders`
+      );
+      orders.forEach((order) => createNode(transformOrderNode(order)));
+    }
 
-    const products = await createQuery(
-      productsQuery,
-      {
-        Variant: 'variants'
-      },
-      options,
-      reporter
-    );
+    if (!queries || queries.products) {
+      const products = await createQuery(
+        productsQuery,
+        {
+          Variant: 'variants'
+        },
+        options,
+        reporter
+      );
 
-    reporter.info(
-      `gatsby-source-shopify-graphql fetched ${products.length} products`
-    );
-    products.forEach((product) => createNode(transformProductNode(product)));
+      reporter.info(
+        `gatsby-source-shopify-graphql fetched ${products.length} products`
+      );
+      products.forEach((product) => createNode(transformProductNode(product)));
+    }
   } catch (error) {
     reporter.panicOnBuild(error);
   }
